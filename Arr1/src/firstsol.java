90e2d0dd82391e744cb382c81ea93fc560e5763a
pc <pc@DESKTOP-C81JPEF>
2020-06-24 23:00:31 +0530

    added

--- Arr1/src/firstsol.java
+++ Arr1/src/firstsol.java
@@ -1,4 +1,22 @@
 
 public class firstsol {
-
-}
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int a[]=new int[20];
+	for(int i=0;i<n;i++)
+	{
+	a[i]=sc.nextInt();
+	}
+	int sum=0,count=0;
+	for(int i=0;i<n;i++)
+	{
+	sum=sum+a[i];
+	count++;
+	}
+	int avg=sum/count;
+	System.out.println("sum is:"+sum);
+	System.out.println("avg is:"+avg);
+	}
+	}
