3b0e063b1ebebc721c7b926de360c7007a8ea560
Jagadish <41832745+jagadish1309@users.noreply.github.com>
2020-07-02 13:21:41 +0530

    First Commit

---
+++ Interfaces/Testpackage.java
@@ -0,0 +1,12 @@
+package Interfaces;
+import Interfaces.veena;
+import Interfaces.saxophone;
+public class Testpackage {
+	public static void main(String[] args)
+	{
+veena ob1=new veena();
+saxophone ob2=new saxophone();
+ob1.play();
+ob2.play();
+	}
+}
