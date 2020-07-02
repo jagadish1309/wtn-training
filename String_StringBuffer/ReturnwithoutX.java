b202041136a93cd906b725cdcd9653768382b4af
Jagadish <41832745+jagadish1309@users.noreply.github.com>
2020-07-02 13:29:52 +0530

    First Commit

---
+++ String_StringBuffer/ReturnwithoutX.java
@@ -0,0 +1,13 @@
+package String_StringBuffer;
+import java.util.*;
+public class ReturnwithoutX {
+	public static void main(String[] args)
+	{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+
+if(s.charAt(0)=='x' && s.charAt(s.length()-1)=='x')
+	System.out.println(s.substring(1,s.length()-1));
+else
+	System.out.println(s);
+}
