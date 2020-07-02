bcf9ba2b59e6d21b0ccf36cfdf41039fe395117d
Jagadish <41832745+jagadish1309@users.noreply.github.com>
2020-07-02 13:28:25 +0530

    First Commit

---
+++ String_StringBuffer/palindrome.java
@@ -0,0 +1,38 @@
+package String_StringBuffer;
+import java.util.*;
+public class palindrome { 
+	  
+   
+    static boolean isPalindrome(String str) 
+    { 
+  
+       
+        int i = 0, j = str.length() - 1; 
+  
+        
+        while (i < j) { 
+  
+            if (str.charAt(i) != str.charAt(j)) 
+                return false; 
+  
+        
+            i++; 
+            j--; 
+        } 
+  
+      
+        return true; 
+    } 
+  
+    
+    public static void main(String[] args) 
+    { 
+    	Scanner sc=new Scanner (System.in);
+        String str =sc.next();
+  
+        if (isPalindrome(str)) 
+            System.out.print("Yes"); 
+        else
+            System.out.print("No"); 
+    } 
+} 
