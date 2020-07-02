c154cfab83e3573905a74838c8cc45df299b0704
Jagadish <41832745+jagadish1309@users.noreply.github.com>
2020-07-02 13:29:10 +0530

    First Commit

---
+++ String_StringBuffer/Repetation.java
@@ -0,0 +1,20 @@
+package String_StringBuffer;
+import java.util.*;
+public class Repetation {
+	public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	String s=sc.next();
+	int n=sc.nextInt();
+	int l=s.length();
+	String sub=s.substring(l-n);
+	String res="";
+	for(int i=1;i<=n;i++)
+	{
+		res+=sub;
+	}
+	System.out.println(res);
+}
+	
+
+}
